98153db0eaf4626d3ad609888a64a340ba6db0f4
Bharathg1997 <85443378+Bharathg1997@users.noreply.github.com>
2021-10-06 11:18:41 +0530

    Update devshift.sh

--- devshift.sh
+++ devshift.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 /usr/local/bin/oc login -u kubeadmin -p r6PzC-4hayC-TjCIg-NaL4m --insecure-skip-tls-verify=true
-x=devopsimage
-y=$(/usr/local/bin/oc get routes | awk '{print $1}' | grep devopsimage)
+x=devops
+y=$(/usr/local/bin/oc get routes | awk '{print $1}' | grep devops)
 if [ "$x" == "$y" ]
 then
     echo "Pod and Services already existed in openshift"
 else
-    /usr/local/bin/oc new-app --image-stream=bharath/devopsimage:v1 -e LICENSE=accept && /usr/local/bin/oc create route passthrough devopsimage --service=devopsimage --port=7843
+    /usr/local/bin/oc new-app --image-stream=bharath/devops:v1 -e LICENSE=accept && /usr/local/bin/oc create route passthrough devops --service=devops --port=7843
 fi
